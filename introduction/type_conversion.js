8da0d4bf99f0498d18de0276679a22135bfb927b
sumit kumar bighaniya <sb78639@gmail.com>
2021-10-06 22:20:11 +0530

    intial commit

---
+++ introduction/type_conversion.js
@@ -0,0 +1,24 @@
+let value = true
+
+console.log(typeof value)
+value = String(value)
+
+console.log(typeof value)
+
+// Numeric Conversion
+
+console.log("6"/"2")
+
+let str = "123"
+console.log(typeof str)
+
+let num = Number(str);
+console.log(typeof num)
+
+
+// boolean conversion
+
+let bool = Boolean(1)
+console.log(bool)
+bool = Boolean(!Number(bool))
+console.log(bool)
