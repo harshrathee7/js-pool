3b440daaf7338216379acc93d7f20d72e1a4f888
sumit kumar bighaniya <sb78639@gmail.com>
2021-10-07 23:21:50 +0530

    basic math & operators updated

---
+++ introduction/basic_math.js
@@ -0,0 +1,69 @@
+// basic math operators
+
+// unary: single operand
+
+let x = 1
+x = -x
+console.log(x)
+
+// binary operator: work with two operands
+
+let a = 10;
+let b = 29;
+let c = a+b
+console.log(c)
+
+// Math Operatot
+/*
+Addition(+),
+Subtraction(-),
+Multiplication(*),
+Division(/),
+Remainder(%),
+Exponential(**)
+*/
+
+// String concatenation
+
+// + can be used for the string concatenation
+
+console.log("sumit "+"kumar")
+
+// Numeric conversion, unary
+
+console.log(+'1'+1) //  Number
+
+
+
+// Assignment = 
+
+let z = 20
+
+console.log(z)
+
+// chaning assignment
+
+let p,q,r;
+p = q = r = 20
+
+console.log(p,q,r)
+
+
+// Increment & Decrement
+
+
+let s = 30
+
+++s
+s++
+console.log(s) // 32
+
+// decrement
+
+s--
+--s
+console.log(s) // 30
+
+
+
+// Bitwise operators:(&,|,^,~,<<,>>,>>>)
