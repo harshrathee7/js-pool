8201e13e36179dbaa4d7ad76b8b8d2ea1e6fcbbb
Ammyy9908 <sb78639@gmail.com>
2021-10-10 11:01:38 +0530

    logical added in fundamentals

---
+++ Fundamentals/logical.js
@@ -0,0 +1,18 @@
+//1 . || Logical OR
+
+console.log(true || false); // true
+console.log(true || true); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+// 2. && Logical AND
+
+console.log(true && false); // false
+console.log(true && true); // true
+console.log(false && false); // false
+console.log(false && true); // false
+
+// 3. Logical NOT (!):called invertor
+
+console.log(!true); // false
+console.log(!false); // true
