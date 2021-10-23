2f2e535ed219d68e14a97496550e8a479a348569
Ammyy9908 <sb78639@gmail.com>
2021-10-23 10:41:53 +0530

    currying & eval added in Part1/Miscellaneous

---
+++ Part1/Miscellaneous/Eval.js
@@ -0,0 +1,14 @@
+// Eval: run a code string
+
+/*The built-in eval function 
+allows to execute a string of code.*/
+
+// the syntax:let result = eval(code)
+
+let code = 'console.log("Hello")';
+
+eval(code);
+
+let sum = "1+2";
+
+console.log(eval(sum));
---
+++ Part1/Miscellaneous/currying.js
@@ -0,0 +1,30 @@
+/*
+{Currying} is an advanced technique of
+ working with functions. It’s used not only in 
+JavaScript, but in other languages as well. 
+
+*/
+
+// e.g
+
+/**
+ * Currying is a transformation of functions
+ * that translates a function from
+ *  callable as f(a, b, c) into callable as f(a)(b)(c)
+ */
+
+function curry(f) {
+  return function (a) {
+    return function (b) {
+      return f(a, b);
+    };
+  };
+}
+
+// usage
+function sum(a, b) {
+  return a + b;
+}
+let curriedSum = curry(sum);
+
+console.log(curriedSum(10)(20)); // 30
